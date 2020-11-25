59d2bc2d6276792b190e4cc45dcf38e5b4ebdf3a
jjchiw <jjchiw@gmail.com>
2020-11-26 00:27:15 +0100

    other clubs

--- dapr_run.sh
+++ dapr_run.sh
@@ -1,1 +1,2 @@
-dapr run --app-id timon-api-gateway --app-port 5002  dotnet run
+#dapr run --app-id "timon-api-gateway" --app-port "5002" --components-path "./components" --dapr-grpc-port "50003" --dapr-http-port "3500" dotnet run
+daprd --app-id "timon-api-gateway" --app-port "5002" --components-path "./components" --dapr-grpc-port "50003" --dapr-http-port "3500" "--enable-metrics=false" --placement-address "localhost:50005"
