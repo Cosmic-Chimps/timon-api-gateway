92587efc44da894d4aee885265f9183b46b6b56a
jjchiw <jjchiw@gmail.com>
2021-03-31 00:28:40 +0200

    Add healtcheck

--- src/TimonApiGateway/Startup.cs
+++ src/TimonApiGateway/Startup.cs
@@ -12,90 +12,97 @@ using Ocelot.Middleware;
 
 namespace TimonApiGateway
 {
-  public class Startup
-  {
-    public Startup(IConfiguration configuration)
+    public class Startup
     {
-      Configuration = configuration;
-    }
-
-    public IConfiguration Configuration { get; }
-
-    // This method gets called by the runtime. Use this method to add services to the container.
-    public void ConfigureServices(IServiceCollection services)
-    {
-      var identityServerConfig = Configuration.GetSection(nameof(IdentityServerOptions));
-
-      var identityServerOptions = new IdentityServerOptions();
-      identityServerConfig.Bind(identityServerOptions);
-
-      var authenticationProviderKey = identityServerOptions.AuthenticationProviderKey;
-
-      services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
-          .AddIdentityServerAuthentication(authenticationProviderKey, options =>
-          {
-                  // base-address of your identityserver
-                  options.Authority = identityServerOptions.Authority;
-                  // name of the API resource
-                  options.ApiName = identityServerOptions.ApiName;
-            options.ApiSecret = identityServerOptions.ApiSecret;
-          });
-      // services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
-      //     .AddJwtBearer(authenticationProviderKey, options =>
-      //     {
-      //         options.Authority = identityServerOptions.Authority;
-      //         options.Audience = identityServerOptions.ApiName;
-      //     });
-
-      // #if DEBUG
-      services.AddCors(options =>
-      {
-        options.AddPolicy("CorsPolicy",
-                  builder => builder.AllowAnyOrigin()
-                      .AllowAnyMethod()
-                      .AllowAnyHeader());
-      });
-      // #else
-      //             var allowedDomains = Configuration["AllowedDomains"];
-      //             services.AddCors(options =>
-      //             {
-      //                 options.AddPolicy("CorsPolicy",
-      //                     builder => builder.WithOrigins("allowedDomains")
-      //                         .AllowAnyMethod()
-      //                         .AllowAnyHeader()
-      //                         .AllowCredentials());
-      //             });
-      // #endif
-
-      services.AddOcelot();
-    }
-
-    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public async void Configure(IApplicationBuilder app,
-                                IWebHostEnvironment env)
-    {
-      var conf = new OcelotPipelineConfiguration()
-      {
-        PreErrorResponderMiddleware = async (ctx, next) =>
+        public Startup(IConfiguration configuration)
         {
-          if (ctx.Request.Path.Equals(new PathString("/")))
-          {
-            await ctx.Response.WriteAsync("ok");
-          }
-          else
-          {
-            await next.Invoke();
-          }
+            Configuration = configuration;
         }
-      };
 
-      if (env.IsDevelopment())
-      {
-        app.UseDeveloperExceptionPage();
-      }
+        public IConfiguration Configuration { get; }
 
-      app.UseCors("CorsPolicy");
-      await app.UseOcelot(conf);
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            var identityServerConfig = Configuration.GetSection(nameof(IdentityServerOptions));
+
+            var identityServerOptions = new IdentityServerOptions();
+            identityServerConfig.Bind(identityServerOptions);
+
+            var authenticationProviderKey = identityServerOptions.AuthenticationProviderKey;
+
+            services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+                .AddIdentityServerAuthentication(authenticationProviderKey, options =>
+                {
+              // base-address of your identityserver
+              options.Authority = identityServerOptions.Authority;
+              // name of the API resource
+              options.ApiName = identityServerOptions.ApiName;
+                    options.ApiSecret = identityServerOptions.ApiSecret;
+                });
+            // services.AddAuthentication(IdentityServerAuthenticationDefaults.AuthenticationScheme)
+            //     .AddJwtBearer(authenticationProviderKey, options =>
+            //     {
+            //         options.Authority = identityServerOptions.Authority;
+            //         options.Audience = identityServerOptions.ApiName;
+            //     });
+
+            // #if DEBUG
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder.AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+
+            services.AddHealthChecks();
+            // #else
+            //             var allowedDomains = Configuration["AllowedDomains"];
+            //             services.AddCors(options =>
+            //             {
+            //                 options.AddPolicy("CorsPolicy",
+            //                     builder => builder.WithOrigins("allowedDomains")
+            //                         .AllowAnyMethod()
+            //                         .AllowAnyHeader()
+            //                         .AllowCredentials());
+            //             });
+            // #endif
+
+            services.AddOcelot();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public async void Configure(IApplicationBuilder app,
+                                    IWebHostEnvironment env)
+        {
+            var conf = new OcelotPipelineConfiguration()
+            {
+                PreErrorResponderMiddleware = async (ctx, next) =>
+                {
+                    if (ctx.Request.Path.Equals(new PathString("/")))
+                    {
+                        await ctx.Response.WriteAsync("ok");
+                    }
+                    else
+                    {
+                        await next.Invoke();
+                    }
+                }
+            };
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapHealthChecks("/health");
+            });
+
+            app.UseCors("CorsPolicy");
+            await app.UseOcelot(conf);
+        }
     }
-  }
 }
